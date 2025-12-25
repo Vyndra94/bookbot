c62279eb5545a83b88e86fb292fe37487496eec5
Vyndra94 <vyndra@t-online.de>
2025-12-25 23:36:53 +0100

    updated README.md and added a sort function to stats.py

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # bookbot
 BookBot is my first [Boot.dev](https://www.boot.dev) project!
 
-main.py - main executable
+main.py - main executable, calls relevant functions from stats.py
 stats.py - contains all functions that main uses
+wordcount - counts the words in the book and prints the value
+characters - counts how often each alphabetical character appears
+sorted_characters - sorts the dictonary from characters
--- stats.py
+++ stats.py
@@ -8,12 +8,16 @@ def characters():
     with open("books/frankenstein.txt") as f:
         lowercase_contents = f.read().lower()
         for char in lowercase_contents:
-            if char in characters:
-                characters[char] += 1
-            else:
-                characters[char] = 1
+            if char.isalpha():
+                if char in characters:
+                    characters[char] += 1
+                else:
+                    characters[char] = 1
         return characters
 
 def sorted_characters():
-    sorted = characters().sort(reverse=True, key=sort_key)
-    return sorted
+    sorted_characters = list({"char": k, "num": v} for k, v in characters().items())
+    def sort_key(sorted_characters):
+        return sorted_characters["num"]
+    sorted_characters.sort(reverse=True, key=sort_key)
+    return sorted_characters
